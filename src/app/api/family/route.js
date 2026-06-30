b6ac84a2688ddc329eba908929c9bc587a392ad5
Vladimir Plesco <vladimir.plesco@gmail.com>
2026-06-30 15:46:22 +0300

    test response

--- src/app/api/family/route.js
+++ src/app/api/family/route.js
@@ -6,27 +6,29 @@ export const dynamic = 'force-dynamic';
 // const reader = createReader(process.cwd(), config);
 
 export async function GET() {
-  try {
-    console.log('cwd:', process.cwd());
-    const reader = createReader(process.cwd(), config);
-    const entries = await reader.collections.people.all();
+  return new Response('HELLO FROM API');
 
-    return NextResponse.json({
-      // success: true,
-      // entries,
-      children: entries.map(({ entry }) => ({
-        id: entry.id,
-        name: entry.name,
-        date: entry.birthDate,
-      })),
-    });
-  } catch (error) {
-    return NextResponse.json(
-      {
-      success: false,
-      error: error.message,
-      },
-      { status: 500 }
-    );
-  }
+  // try {
+  //   console.log('cwd:', process.cwd());
+  //   const reader = createReader(process.cwd(), config);
+  //   const entries = await reader.collections.people.all();
+
+  //   return NextResponse.json({
+  //     // success: true,
+  //     // entries,
+  //     children: entries.map(({ entry }) => ({
+  //       id: entry.id,
+  //       name: entry.name,
+  //       date: entry.birthDate,
+  //     })),
+  //   });
+  // } catch (error) {
+  //   return NextResponse.json(
+  //     {
+  //     success: false,
+  //     error: error.message,
+  //     },
+  //     { status: 500 }
+  //   );
+  // }
 }
