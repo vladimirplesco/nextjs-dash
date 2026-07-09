ad678f96dc5a8d2f471a175087abfe06a442e153
Vladimir Plesco <vladimir.plesco@gmail.com>
2026-07-09 19:03:00 +0300

    new log secret

--- src/app/api/keystatic/[...params]/route.js
+++ src/app/api/keystatic/[...params]/route.js
@@ -5,6 +5,15 @@ import config from '../../../../../keystatic.config.js';
 console.log('CLIENT_ID =', process.env.KEYSTATIC_GITHUB_CLIENT_ID);
 console.log('CLIENT_SECRET exists =', !!process.env.KEYSTATIC_GITHUB_CLIENT_SECRET);
 console.log('SECRET exists =', !!process.env.KEYSTATIC_SECRET);
+console.log(
+  'KEYSTATIC CLIENT ID:',
+  process.env.KEYSTATIC_GITHUB_CLIENT_ID
+);
+
+console.log(
+  'KEYSTATIC SECRET LENGTH:',
+  process.env.KEYSTATIC_GITHUB_CLIENT_SECRET?.length
+);
 
 export const { GET, POST } = makeRouteHandler({
   config,
